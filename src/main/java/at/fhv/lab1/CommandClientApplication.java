84759ddba906143c673de419896ea75057dc547b
Julian Hafner <jha4540@students.fhv.at>
2024-04-06 10:54:43 +0200

    initialize

--- src/main/java/at/fhv/lab1/CommandClientApplication.java
+++ src/main/java/at/fhv/lab1/CommandClientApplication.java
@@ -35,10 +35,12 @@ public class CommandClientApplication {
             System.out.println("Result: " + publisher.publishEvent(event));
 
             BookRoomEvent rbevent = new BookRoomEvent();
+            /*
             rbevent.setCustomer("Test");
             rbevent.setDuration(12343214);
             rbevent.setBookingID(1);
             rbevent.setRoomID(5);
+            */
             publisher.publishEvent(rbevent);
         };
     }
